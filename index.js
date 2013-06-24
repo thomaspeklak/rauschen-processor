1b5610c4febe5c18ede5ddfa36ea2a9837ca1759
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-24 23:51:16 +0200

    use registry to connect to receiver

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 "use strict";
 
-var net         = require("net");
+var packageInfo = require("./package");
+var net = require("net");
 var Scuttlebutt = require("scuttlebutt/model");
 var timing = new Scuttlebutt();
-var es          = require("event-stream");
-var fs          = require("fs");
-var stringify   = require("JSONStream").stringify;
 var socket = require("./config").sockets.receiver;
-var persistenceStream    = require("./lib/persistence-stream");
+//var persistenceStream = require("./lib/persistence-stream");
 var dataExtractionStream = require("./lib/data-extraction-stream");
-var dataEnrichStream     = require("./lib/data-enrich-stream");
-var socketStream         = require("./lib/socket-stream");
+var dataEnrichStream = require("./lib/data-enrich-stream");
 
 var timingStream = timing.createStream();
 var enrichedStream = timingStream.pipe(dataExtractionStream()).pipe(dataEnrichStream());
-enrichedStream.pipe(persistenceStream);
-var serializedStream = enrichedStream.pipe(stringify(false));
+//enrichedStream.pipe(persistenceStream);
 
-socketStream(function(stream) {
-    serializedStream.pipe(stream);
-});
+//@TODO refactor to use seaport
+//used for RTA
+//var socketStream         = require("./lib/socket-stream");
+//var stringify = require("JSONStream").stringify;
+//var serializedStream = enrichedStream.pipe(stringify(false));
+//socketStream(function(stream) {
+//serializedStream.pipe(stream);
+//});
 
 //serializedStream.pipe(process.stdout);
 //timingStream.pipe(process.stdout);
-
-var delayInitilization = function () {
-    console.log("processor trying to connect to receiver");
-    setTimeout(initilizeStream, 1000);
-};
 
 var msg = function (message) {
     return function () {
@@ -35,25 +31,38 @@ var msg = function (message) {
     };
 };
 
+var seaport = require("seaport");
+var seaportConf = {
+    host: process.argv[2],
+    port: parseInt(process.argv[3], 10)
+};
+var ports = seaport.connect(seaportConf.host, seaportConf.port);
+
+//var client = require("rauschen-registry").client;
+//var config = client(seaportConf.host, seaportConf.port);
+//config.on("update", function () {});
+
 var initilizeStream = function () {
-    if (!fs.existsSync(socket)) {
-        return delayInitilization();
-    }
+    ports.get("distributor@" + packageInfo.peerDependencies["rauschen-receiver"], function (ps) {
+        var distributor = ps[0];
 
-    var connection = net.connect(socket);
-    var cleanupAndInitialize = function () {
-        connection.removeAllListeners();
-        delayInitilization();
-    };
+        var connection = net.connect(distributor);
 
-    connection.on("connect", msg("connecting to receiver"));
-    connection.on("timeout", msg("stream timeout"));
-    connection.on("close", cleanupAndInitialize);
+        var cleanupAndInitialize = function () {
+            connection.removeAllListeners();
+            initilizeStream();
+        };
 
-    timingStream.pipe(connection).pipe(timingStream, {end: false});
+        connection.on("connect", msg("connecting to receiver"));
+        connection.on("timeout", msg("stream timeout"));
+        connection.on("close", cleanupAndInitialize);
+
+        timingStream.pipe(connection).pipe(timingStream, {
+            end: false
+        });
+    });
 };
 
 initilizeStream();
 
 process.send && process.send("processor turned on");
-
